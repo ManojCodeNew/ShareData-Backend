7727ab299c68f45a1ec0095883f91bf1fef71854
manoj <manojpoojary2004@gmail.com>
2025-09-16 00:22:44 +0530

    Initial backend commit

---
+++ Server.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import Message from './models/Message.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const connect = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://manojc2004:WbswPxuS9gkIsFQ0@clusterdatashare.zafzitl.mongodb.net/")
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+
+const genrateToken = () => {
+    return Math.floor(1000 + Math.random() * 9000);
+}
+app.post('/api/message', async (req, res) => {
+    const { message } = req.body;
+    if (!message) {
+        return res.status(400).json({ message: "Message is required" });
+    }
+    const newMessage = new Message({ message, token: genrateToken() });
+    await newMessage.save();
+    res.json({ message: "Message saved successfully", token: newMessage.token });
+})
+
+app.get('/api/message/:token', async (req, res) => {
+    try {
+        const token = req.params.token;
+        if (!token) {
+            res.status(400).json({ message: "Error: Token is required" });
+        }
+        const message = await Message.findOne({ token });
+        if (!message) {
+            return res.status(404).json({ message: "Error: Message not found" });
+        }
+        res.json({ message: message.message });
+    } catch (error) {
+        res.status(500).json({ message: "Error: Something went wrong" });
+    }
+})
+
+app.listen(5000, () => {
+    connect();
+    console.log("Server is running on port 5000");
+});
